ebffd5adb7a67e1d306f00291134d76cb86997f2
cmvcordova <cmvcordova@pm.me>
2023-05-08 22:28:49 -0400

    added function to parse ACNet single line json

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,16 +1,26 @@
-import torch
-import JSON
+import json
 import pandas as pd
 
-def read_ACNet_single_line_JSON_datset(path_to_file: str, targets: int) -> pd.DataFrame:
+def read_ACNet_single_line_JSON_file(path_to_json_file: str) -> pd.DataFrame:
     """
-    Read MMP dataset from JSON file as provided after generating ACNet datasets.
-    as detailed in https://github.com/DrugAI/ACNet#usage
+    Read MMP dataset from JSON file as provided after generating ACNet datasets
+    as generated from https://github.com/DrugAI/ACNet#usage after running
+    GenerateACDatasets and convert it to a pandas DataFrame.
+    
+    GenerateACDatasets generates a single line JSON file, delimiting MMPs by a comma.
+    
     Args:
         input_csv (str): Path to the JSON file.
     Returns:
         pd.DataFrame: MMP dataset.
     """
-    df = print(path_to_file)
-    #df = pd.read_csv(input_csv)
+    df_list = []
+    with open(path_to_json_file) as source:
+        data = json.load(source)
+
+    for key in data.keys():
+        _df = pd.json_normalize(data, record_path=[key])
+        _df['Target'] = key
+        df_list.append(_df)
+    df = pd.concat(df_list)
     return df
