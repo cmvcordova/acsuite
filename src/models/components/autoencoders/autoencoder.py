6a4e1489f76c4e76a7d0e8321f07e2c5c68d523c
cmvcordova <cmvcordova@pm.me>
2023-06-22 07:57:05 -0400

    updated requirements file

--- src/models/components/autoencoders/autoencoder.py
+++ src/models/components/autoencoders/autoencoder.py
@@ -7,36 +7,36 @@ import torch.nn.functional as F
 class AutoEncoder(nn.Module):
 
     def __init__(self, in_features: int = 1024, code_features: int = 64,
-    depth: int = 2, activation: nn.Module = nn.ReLU()):
+        depth: int = 2, activation: nn.Module = nn.ReLU()):
 
-    """
-    Autoencoder for molecular data. Takes ECFP-like features as input and 
-    outputs a compressed fingerprint as the autoencoder's latent code.
+        """
+        Autoencoder for molecular data. Takes ECFP-like features as input and 
+        outputs a compressed fingerprint as the autoencoder's latent code.
 
-    Args:
-        in_features (int): Number of input features, ECFP Size
-        code_features (int): Number of features in the code, i.e. size of the compressed fingerprint
-        activation (nn.Module, optional): Activation function to use for the code. Defaults to nn.Sigmoid().
-    """
-    super().__init__()
+        Args:
+            in_features (int): Number of input features, ECFP Size
+            code_features (int): Number of features in the code, i.e. size of the compressed fingerprint
+            activation (nn.Module, optional): Activation function to use for the code. Defaults to nn.Sigmoid().
+        """
+        super().__init__()
 
-    assert in_features > code_features "Input features must be greater than code features"
+        assert in_features > code_features, f"Input features must be greater than code features: {in_features} !> {code_features}"
 
-    self.encoder = nn.Sequential(
-        nn.Linear(in_features, in_features/2), # input layer
-        activation,
-        nn.Linear(in_features/2, in_features/4), ## hidden 1
-        activation,
-        nn.Linear(in_features/4, code_features), ## hidden 2, code output layer
-    )
-    
-    self.decoder = nn.Sequential(
-        nn.Linear(code_features, in_features/4), ## hidden 1,
-        activation,
-        nn.Linear(in_features/4, in_features/2), ## hidden 2
-        activation,
-        nn.Linear(in_features/2, in_features)
-     ) ## output layer, restore original FP size
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features, in_features/2), # input layer
+            activation,
+            nn.Linear(in_features/2, in_features/4), ## hidden 1
+            activation,
+            nn.Linear(in_features/4, code_features), ## hidden 2, code output layer
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(code_features, in_features/4), ## hidden 1,
+            activation,
+            nn.Linear(in_features/4, in_features/2), ## hidden 2
+            activation,
+            nn.Linear(in_features/2, in_features)
+         ) ## output layer, restore original FP size
 
     def forward_encoder(self, x):
         z = self.encoder(x)
@@ -48,7 +48,7 @@ class AutoEncoder(nn.Module):
     
     def forward(self, x):
         z = self.forward_encoder(x)
-        logits = self.forward_decoder(_x)
+        logits = self.forward_decoder(z)
         return logits
     
     #def initialize_weights(self):
